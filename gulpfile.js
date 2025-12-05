2ba076ebadf02585d8f8f3f12e6c1a4e9aa5bb29
Felipe <Felipe@teste.com.br>
2025-12-05 20:25:17 -0300

    config uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const cleanCSS = require('gulp-clean-css');
 const replace = require('gulp-replace');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() {
     return gulp.src('src/styles/main.scss')
@@ -18,8 +25,9 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
-    gulp.watch('src/styles/**/*.scss', gulp.parallel(styles));
+    gulp.watch('src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('src/scripts/*.scss', gulp.parallel(scripts));
 }
